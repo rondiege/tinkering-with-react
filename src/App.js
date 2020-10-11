fe72f6a669dfd2ed1d80b68675b4bdd9e6dc313f
rachelle <rachelle@modopayments.com>
2020-10-11 08:53:24 -0500

    Ternary if statements

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import Person from './Person/Person';
 
@@ -7,17 +7,21 @@ class App extends Component {
         persons: [
             {name: 'Max', age: 8},
             {name: 'Grant', age: 6}
-        ]
+        ],
+        otherstate: 'some other value',
+        showPersons: false
     }
 
     switchNameHandler = (newName) => {
-        this.setState({persons: [
-            {name: 'Maxwell', age: 24},
-            {name: newName, age: 26}
-        ]})
+        this.setState({
+            persons: [
+                {name: 'Maxwell', age: 24},
+                {name: newName, age: 26}
+            ]
+        })
     }
 
-    nameChangeHandler = (event )=> {
+    nameChangeHandler = (event) => {
         this.setState({
             persons: [
                 {name: event.target.value, age: 24},
@@ -25,6 +29,15 @@ class App extends Component {
             ]
         })
     }
+
+    togglePersonsHandler = () => {
+        const doesShow = this.state.showPersons;
+        // setState does not overwrite, just merges
+        this.setState({showPersons: !doesShow})
+    }
+
+
+
     render() {
 
         const style = {
@@ -38,15 +51,24 @@ class App extends Component {
         return (
             <div className="App">
                 <h1>Hi! I am a react app!!</h1>
-                <Person name={this.state.persons[0].name} change={this.nameChangeHandler}/>
-                {/*Anonymous functions like this are less efficient, better to us the bind methodology*/}
-                <Person name="Jesse" click={() => this.switchNameHandler("grant")}/>
-                <Person name={this.state.persons[1].name} click={this.switchNameHandler.bind(this, "Lightning Thunder")}>I like geckos</Person>
-                {/*passing function by reference so note no () on switchNameHandler. It would execute right away if we did that
+                {/*Ternary if statement, if blocks will not work.
+                so this is if showPersons ? the display them : else null  */}
+                {this.state.showPersons ?
+                    <div>
+                    <Person name={this.state.persons[0].name} change={this.nameChangeHandler}/>
+                    {/*Anonymous functions like this are less efficient, better to us the bind methodology*/}
+                    <Person name="Jesse" click={() => this.switchNameHandler("grant")}/>
+                    <Person name={this.state.persons[1].name}
+                            click={this.switchNameHandler.bind(this, "Lightning Thunder")}>I like geckos</Person>
+                    {/*passing function by reference so note no () on switchNameHandler. It would execute right away if we did that
                 Also note capitalized C in onClick, unlike usual*/}
+                </div> : null}
                 <button
                     style={style}
-                    onClick={this.switchNameHandler.bind(this, 'Grant-eous')}>Click Me</button>
+                    // onClick={this.switchNameHandler.bind(this, 'Grant-eous')}
+                    onClick={this.togglePersonsHandler}>
+                    Click Me
+                </button>
             </div>
         );
         // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
